cd8dd3d8e86123e82188e518962e528e43d0e7fa
Van Pho Nguyen <vanpho02@gmail.com>
2018-01-22 20:07:08 +0700

    a.js

---
+++ a.js
@@ -0,0 +1,29 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    sayHello() {
+        console.log(`Hello, I am ${this.name}`);
+    }
+}
+
+class Child extends Person {
+    getAgeOfPerson() {
+        console.log(`${this.name} is ${this.age} years old.`);
+    }
+
+    static compare(p1, p2) {
+        const message = p1.age > p2.age ? `${p1.name} is bigger than ${p2.name}` : `${p2.name} bigger than ${p1.name}`
+        console.log(message);
+    }
+}
+
+const teo = new Child('Teo Nguyen', 10);
+const ti = new Child('Ti Nguyen', 12);
+Child.compare(teo, ti);
+// teo.getAgeOfPerson();
+// console.log(teo);
+// teo.sayHello();
+// teo.getAgeOfPerson();
